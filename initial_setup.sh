f0714fabebcfa7c6b271a42ddc8b438f2a3cba33
Riley Avron <riley.avron@gmail.com>
2018-06-23 10:21:33 -0700

    More initial setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,9 +7,29 @@ set -o pipefail
 
 readonly FGID=845
 readonly FUID=845
+readonly TMP_LOC="/tmp/factorio.tar.xz"
 
 # Create group and user if not already present
 sudo groupadd --gid "$FGID" --system factorio || true
 sudo useradd --uid "$FUID" --gid "$FGID" --system factorio || true
+
+# Create install and data locations
+sudo mkdir --parents /opt/factorio /factorio
+sudo chown --recursive factorio:factorio /opt/factorio /factorio
+
+# Switch to factorio
+sudo su - factorio
+
+# Pull down and unpack the latest stable server
+curl --location https://www.factorio.com/get-download/stable/headless/linux64 \
+    --output "$TMP_LOC"
+# Expand into /opt/factorio
+tar --extract --file "$TMP_LOC" --directory /opt
+
+# Link data files into the data location
+ln -s /factorio/saves /opt/factorio/saves
+ln -s /factorio/mods /opt/factorio/mods
+ln -s /factorio/scenarios /opt/factorio/scenarios
+ln -s /factorio/script-output /opt/factorio/script-output
 
 
