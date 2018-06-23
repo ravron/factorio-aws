75d2a6a0ee68717c8ecaa0fbb6f975bce1d2c9fe
Riley Avron <riley.avron@gmail.com>
2018-06-23 10:33:00 -0700

    Move some stuff to factorio_setup.sh

---
+++ factorio_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
+
+# This script should be run as user factorio
+
+readonly TMP_LOC="/tmp/factorio.tar.xz"
+
+# Pull down and unpack the latest stable server
+curl --location https://www.factorio.com/get-download/stable/headless/linux64 \
+    --output "$TMP_LOC"
+# Expand into /opt/factorio
+tar --extract --file "$TMP_LOC" --directory /opt
+
+# Link data files into the data location
+ln -s /factorio/saves /opt/factorio/saves
+ln -s /factorio/mods /opt/factorio/mods
+ln -s /factorio/scenarios /opt/factorio/scenarios
+ln -s /factorio/script-output /opt/factorio/script-output
--- initial_setup.sh
+++ initial_setup.sh
@@ -5,9 +5,9 @@ set -o pipefail
 
 # Mostly taken from https://github.com/dtandersen/docker_factorio_server/blob/master/0.16/Dockerfile
 
+readonly SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly FGID=845
 readonly FUID=845
-readonly TMP_LOC="/tmp/factorio.tar.xz"
 
 # Create group and user if not already present
 sudo groupadd --gid "$FGID" --system factorio || true
@@ -18,18 +18,4 @@ sudo mkdir --parents /opt/factorio /factorio
 sudo chown --recursive factorio:factorio /opt/factorio /factorio
 
 # Switch to factorio
-sudo su - factorio
-
-# Pull down and unpack the latest stable server
-curl --location https://www.factorio.com/get-download/stable/headless/linux64 \
-    --output "$TMP_LOC"
-# Expand into /opt/factorio
-tar --extract --file "$TMP_LOC" --directory /opt
-
-# Link data files into the data location
-ln -s /factorio/saves /opt/factorio/saves
-ln -s /factorio/mods /opt/factorio/mods
-ln -s /factorio/scenarios /opt/factorio/scenarios
-ln -s /factorio/script-output /opt/factorio/script-output
-
-
+sudo su - factorio "$SCRIPT_LOC"/factorio_setup.sh
