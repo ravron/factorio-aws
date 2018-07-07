f197a713d1e6f578277d1c71283abc33cd1b1126
Riley Avron <riley.avron@gmail.com>
2018-07-06 21:16:01 -0700

    Make main.go actually shutdown

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+    "os"
+    "os/exec"
+    "time"
 
 	"github.com/gtaylor/factorio-rcon"
 )
@@ -13,6 +16,36 @@ func getPassword(filename string) string {
 		panic(err)
 	}
 	return string(b)
+}
+
+const filename = "/factorio/_last-check"
+
+func lastZeroDate() time.Time {
+	b, err := ioutil.ReadFile(filename)
+	if os.IsNotExist(err) {
+		// Return zero time, January 1 year 1
+		return time.Time{}
+	} else if err != nil {
+		panic(err)
+	}
+
+	t := time.Time{}
+	err = t.UnmarshalText(b)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func writeNow() {
+	t, err := time.Now().UTC().MarshalText()
+	if err != nil {
+		panic(err)
+	}
+
+	if err = ioutil.WriteFile(filename, t, 0777); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -39,5 +72,27 @@ func main() {
 		}
 	}
 
-	fmt.Printf("%d\n", online)
+	fmt.Printf("%d players online\n", online)
+
+    // If there are any players, delete file and exit.
+    if online > 0 {
+        // Ignore errors
+        os.Remove(filename)
+        return
+    }
+
+	last := lastZeroDate()
+	thirtyFiveMinAgo := time.Now().UTC().Add(-35 * time.Minute)
+
+	if last.Before(thirtyFiveMinAgo) {
+		// Last check was longer than thirty five minutes ago. Write now to
+		// file and exit.
+		writeNow()
+		return
+	}
+
+	// Last check was less than thirty five minutes ago. Shutdown in one
+	// minute!
+	c := exec.Command("shutdown", "-H", "+1")
+	c.Run()
 }
