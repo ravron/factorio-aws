31120b35cbdeebfbdb6e578f6bde89f91cbeda61
Riley Avron <riley.avron@gmail.com>
2018-07-23 16:40:28 -0700

    Add the lambda handler

---
+++ lambda_handler.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+var instId = "i-0329f22f9ea8e12bc"
+
+func describeInstance(icon string, state string, dns string) string {
+	if dns == "" {
+		return fmt.Sprintf("%s Server is %s and does not have a public DNS name", icon, state)
+	}
+	return fmt.Sprintf("%s Server is %s and has public DNS name %s", icon, state, dns)
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-1")})
+	if err != nil {
+		fmt.Println("failed to create session")
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	client := ec2.New(sess)
+
+	instanceFilter := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{
+			&instId,
+		},
+	}
+
+	desc, err := client.DescribeInstances(instanceFilter)
+	if err != nil {
+		fmt.Println("failed to describe instances")
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	instance := desc.Reservations[0].Instances[0]
+	state := *instance.State.Name
+	dns := *instance.PublicDnsName
+
+	switch state {
+	case "running":
+		return events.APIGatewayProxyResponse{
+			Body:       describeInstance("✅", state, dns),
+			StatusCode: 200,
+		}, nil
+	case "pending":
+		return events.APIGatewayProxyResponse{
+			Body:       describeInstance("⏳", state, dns),
+			StatusCode: 200,
+		}, nil
+	}
+
+	fmt.Printf("starting instance %s\n", instId)
+	_, err = client.StartInstances(&ec2.StartInstancesInput{
+		InstanceIds: []*string{
+			&instId,
+		},
+	})
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "❌ Failed to start instance. Instance is likely transitioning between states. Try again shortly.",
+			StatusCode: 200,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "⏳ Instance is starting, refresh this page to see status",
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
